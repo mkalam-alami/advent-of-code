4afae5b4a39fb637cb86e78f9d332e80d79c563a
Marwane Kalam-Alami <marwane.ka@gmail.com>
2016-12-08 22:25:05 +0100

    2016-08

--- 2016-08.js
+++ 2016-08.js
@@ -2,9 +2,9 @@ var u = require('./utils');
 
 // WIP
 
-var screen = u.createGrid(50, 6, 0);
+var screen = u.createGrid(50, 6, ' ');
 
-u.mockInput('rect 1x1\nrotate row y=0 by 10');
+//u.mockInput('rect 1x1\nrotate row y=0 by 10');
 
 u.readInputAsLines(function(line) {
 	var a = line.split(/[ =]/g);
@@ -12,7 +12,7 @@ u.readInputAsLines(function(line) {
 		var coords = a[1].split('x');
 		for (var i = 0; i < parseInt(coords[0]); i++) {
 			for (var j = 0; j < parseInt(coords[1]); j++) {
-				screen[i][j] = 1;
+				screen[i][j] = '*';
 			}
 		}
 	}
@@ -32,20 +32,31 @@ function rotateScreen(row, col, amount) {
 	var fromJ = (col !== false) ? 0 : row, toJ = (col !== false) ? screen[0].length-1 : row;
 	var di = (row !== false) ? 1 : 0;
 	var dj = (col !== false) ? 1 : 0;
-	//console.log(fromI, fromJ, toI, toJ, "/",di, dj)
 		
 	for (var n = 0; n < amount; n++) {
-		for (var i = toI; i >= fromI; i--) {
-			for (var j = toJ; j >= fromJ; j--) {
-				var newI = (i + di) % screen.length;
-				var newJ = (j + dj) % screen[0].length;
-				console.log(i,j,"<>",newI,newJ);
-				var buffer = screen[newI][newJ];
-				screen[newI][newJ] = screen[i][j];
-				screen[i][j] = buffer;
+		var buffer = false;
+		for (var i = fromI; i <= toI; i++) {
+			for (var j = fromJ; j <= toJ; j++) {
+				if (buffer === false) {
+					buffer = screen[i][j];
+				}
+				else {
+					var otherBuffer = screen[i][j];
+					screen[i][j] = buffer;
+					buffer = otherBuffer;
+				}
 			}	
 		}
+		screen[fromI][fromJ] = buffer;
 	}
 }
 
-console.log(screen, u.countGrid(screen, 1));
+for (var j = 0; j < screen[0].length; j++) {
+	var str = ''
+	for (var i = 0; i < screen.length; i++) {
+		str += screen[i][j];
+	}
+	console.log(str);
+}
+
+console.log(u.countGrid(screen, '*'));
