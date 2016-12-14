838d34ca8c39f9623d72b06f4a06c954deddec7c
Marwane <marwane.ka@gmail.com>
2016-12-14 12:00:30 +0100

    pt. 2 fixed

--- 2016-09.js
+++ 2016-09.js
@@ -2,48 +2,68 @@ var u = require('./utils');
 
 u.readInput(function(line) {
 	line = line.replace(/[ \r\n]/g, '');
-	console.log(process(line, false));
-	console.log(process(line, true));
+	part1(line);
+	part2(line);
 });
 
-function process(line, part2) {
-	var result = 0, resultString = '';;
+function part1(line) {
+	var result = 0;
 	var c;
 	for (var i = 0; i < line.length; i++) {
 		c = line[i];
 		if (c == '(') {
-			if (part2) {
-				resultString += expand(line, i, part2);
-			}
-			else {
-				result += expand(line, i, part2).length;
-			}
-			i = line.indexOf(')', i) + getMarkerInfo(line, i)[0];
+			result += expand(line, i).length;
+			i = getMarkerEndIndex(line, i) + getMarkerInfo(line, i).size;
 		}
 		else {
 			result++;
 		}
 	}
-	return part2 ? resultString : result;
+	console.log(result);
 }
 
-function getMarkerInfo(line, i) {
-	var nextDataI = line.indexOf(')', i);
-	var marker = line.slice(i + 1, nextDataI);
-	var markerInfo = marker.split('x');
-	return markerInfo.map(function(s) { return parseInt(s); });
+function part2(line) {
+	console.log(walk(line, 1));
 }
 
-function expand(line, i, recursive) {
-	var nextDataI = line.indexOf(')', i);
-	var markerInfo = getMarkerInfo(line, i);
-	var data = line.slice(nextDataI + 1, nextDataI + 1 + parseInt(markerInfo[0]));
-	if (recursive && data.indexOf('(') != -1) {
-		data = process(data, true, true);
+function walk(string, multiplier) {
+	var result = 0;
+	for (var i = 0; i < string.length; i++) {
+		c = string[i];
+		if (c == '(') {
+			var markerEndIndex = getMarkerEndIndex(string, i);
+			var markerInfo = getMarkerInfo(string, i);
+			var newMultiplier = multiplier * markerInfo.multiplier;
+			result += walk(string.slice(markerEndIndex + 1, markerEndIndex + 1 + markerInfo.size), newMultiplier);
+			i = markerEndIndex + markerInfo.size;
+		}
+		else {
+			result += multiplier;
+		}
 	}
+	return result;
+}
+
+function getMarkerEndIndex(line, i) {
+	return line.indexOf(')', i);
+}
+
+function expand(line, i) {
+	var markerEndIndex = getMarkerEndIndex(line, i);
+	var markerInfo = getMarkerInfo(line, i);
+	var data = line.slice(markerEndIndex + 1, markerEndIndex + 1 + markerInfo.size);
 	var dupedString = '';
-	for (var n = 0; n < markerInfo[1]; n++) {
+	for (var n = 0; n < markerInfo.multiplier; n++) {
 		dupedString += data;
 	}
 	return dupedString;
+}
+
+function getMarkerInfo(line, i) {
+	var markerString = line.slice(i + 1, getMarkerEndIndex(line, i));
+	var markerArray = markerString.split('x');
+	return {
+		size: parseInt(markerArray[0]),
+		multiplier: parseInt(markerArray[1])
+	};
 }
