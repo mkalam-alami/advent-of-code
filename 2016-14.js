13b6a1ec7bab7fd845390da15737b2ba8aa4d578
Marwane <marwane.ka@gmail.com>
2016-12-21 12:40:30 +0100

    day 14

--- 2016-14.js
+++ 2016-14.js
@@ -6,22 +6,31 @@ let keysFound = 0
 let i = 0
 let result = null
 
+let stretchedMode = true // pt. 2
+
 while (result === null) {
-	let hash = md5('cuanljph' + i++) // cuanljph
+	let hash = md5('cuanljph' + i++)
+	if (stretchedMode) {
+		for (let i = 0; i < 2016; i++) {
+			hash = md5(hash)
+		}
+	}
+	
 	hashes.push(hash)
 	
-	if (i >= 1000) {
-		let candidateIndex = i - 1000
+	if (i > 1000) {
+		let candidateIndex = i - 1001
 		let candidate = hashes[candidateIndex]
 		let token = findRepeating(candidate, 3)
 		if (token) {
 			for (let j = candidateIndex + 1; j <= candidateIndex + 1000; j++) {
 				if (findRepeating(hashes[j], 5, token)) {
 					keysFound++
+					console.log(`key ${keysFound} is ${candidateIndex}: ${candidate} with token ${token}`)
 					if (keysFound === 64) {
 						result = candidateIndex
-						break
 					}
+					break
 				}
 			}
 		}
@@ -34,9 +43,11 @@ function findRepeating(str, size, token) {
 	let foundCount
 	for (let c of Array.from(str)) {
 		if (c === foundToken) {
-			if (++foundCount === size) return foundToken
-		} else if (token === undefined || token === c) {
-			foundToken = c
+			if (++foundCount === size) {
+				return foundToken
+			}
+		} else {
+			foundToken = (token === undefined || token === c) ? c : null
 			foundCount = 1
 		}
 	}
