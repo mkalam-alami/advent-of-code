31c9e9f2442330fbc46c71413402b52aa12de280
Marwane Kalam-Alami <marwane.ka@gmail.com>
2017-11-29 22:13:13 +0100

    2015 day 8

--- 2015/2015-08.js
+++ 2015/2015-08.js
@@ -1,13 +1,20 @@
 var u = require('../utils')
 
 var total = 0
-
-// WORK IN PROGRESS
+var total2 = 0
 
 u.readInputAsLines(function (line) {
   mem = line.slice(1, line.length - 1)
-  console.log(line)
-  u.exit()
+  total += line.length - eval(line).length
+  
+  let subtotal2 = 2
+  for (let c of line) {
+	  if (c === '"' || c === '\\') subtotal2++
+  }
+  console.log(line + " => " + subtotal2)
+  total2 += subtotal2
 })
 
+console.log("--------------")
 console.log(total)
+console.log(total2)
