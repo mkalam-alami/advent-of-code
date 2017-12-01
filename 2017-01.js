53b3bf3021a1dbfe1a0881c9dea84c7ae3cf0fb3
Marwane Kalam-Alami <marwane.ka@gmail.com>
2017-12-01 22:02:23 +0100

    2017 day 1

---
+++ 2017-01.js
@@ -0,0 +1,27 @@
+'use strict'
+
+// Source: https://twitter.com/_sorceress/status/935256907408867328
+
+let u = require('./utils')
+
+let sum = 3
+let sum2 = 0
+
+let prev = -1
+
+u.readInput(function (line) {
+  let offset = line.length / 2
+  let i = 0
+
+  for (let c of line) {
+    if (prev === c) sum += parseInt(c)
+    prev = c
+
+    let opposite = line[(i + offset) % line.length]
+    if (prev === opposite) sum2 += parseInt(c)
+    i++
+  }
+})
+
+console.log("Sum: " + sum)
+console.log("Sum2: " + sum2)
