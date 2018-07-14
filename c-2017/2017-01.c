63653538ceb1a2bf379c610fef58e8d00481ea76
Marwane Kalam-Alami <marwane.ka@gmail.com>
2018-07-14 16:35:48 +0200

    2017-01 c part2

--- c-2017/2017-01.c
+++ c-2017/2017-01.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
+#define FILE_SIZE 2030
+
 int main(void)
 {
     FILE *fp = fopen("../2017-01.txt", "r");
 
+    // Part 1
+    int part1 = 0;
     char first = fgetc(fp);
     char current = first, next = -1;
-    int part1 = 0;
-
     while ((next = fgetc(fp)) != EOF)
     {
         if (current == next) 
@@ -21,9 +23,27 @@ int main(void)
         part1 += current - '0';
     }
 
-    fclose(fp);
+    // Part 1
+    int part2 = 0;
+    fpos_t currentPos = 0;
 
-    printf("Part 1: %d", part1);
+    rewind(fp);
+    for (int i = 0; i < FILE_SIZE / 2; i++) {
+        current = fgetc(fp);
+        fseek(fp, FILE_SIZE / 2 - 1, SEEK_CUR);
+        next = fgetc(fp);
+        fseek(fp, - FILE_SIZE / 2, SEEK_CUR);
+
+        if (current == next) {
+            part2 += (current - '0') * 2;
+        }
+    }
+
+    // Output
+    printf("Part1: %d\n", part1);
+    printf("Part2: %d\n", part2);
+
+    fclose(fp);
 
     return 0;
 }
